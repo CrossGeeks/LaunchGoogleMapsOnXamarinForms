6dbbab57fab1ff77552f6aa9822b99bfc0da1083
Charlin Agramonte <char0394@gmail.com>
2019-01-28 15:40:55 -0400

    Adding project

---
+++ LaunchingGoogleMapSample/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace LaunchingGoogleMapSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public async void Handle_Clicked(object sender, EventArgs e)
+        {
+            double latitud = 40.765819;
+            double longitud = -73.975866;
+            string placeName = "Home";
+
+            var supportsUri = await Launcher.CanOpenAsync("comgooglemaps://");
+
+             if (supportsUri)
+                await Launcher.OpenAsync($"comgooglemaps://?q={latitud},{longitud}({placeName})");
+             else
+                await Map.OpenAsync(40.765819, -73.975866, new MapLaunchOptions { Name = "Test" });
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# LaunchGoogleMapsOnXamarinForms
+# Launch Google Maps On XamarinForms
 A sample of how to open Google Maps on Xamarin Forms
+
+<p align="center">
+<img width="300" height:"700" src="maps.gif" title="iOS"/>
+</p>
+
+
+Blog post: 
+
